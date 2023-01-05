915994074732369728f60b3578c5da8bc5b1ba56
wanjiban <109531247+wanjiban@users.noreply.github.com>
2023-01-05 11:57:47 +0800

    Update diy-part2-k6.1.sh

--- diy-part2-k6.1.sh
+++ diy-part2-k6.1.sh
@@ -10,10 +10,44 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+########### 更改大雕源码（可选）###########
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
+
+
+# 修改生成的固件名称include/image.mk
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX:=$(shell date +%Y%m%d)' include/image.mk
+sed -i '/IMG_PREFIX:=/s/^#\?/#/' include/image.mk
+sed -i '/IMG_PREFIX_VERCODE:=/a\IMG_PREFIX:=wayos-k6.1-$(BUILD_DATE_PREFIX)' include/image.mk 
+
+# WAN LAN LAN LAN && NO V6
+sed -i "/uci commit system/a uci commit network"  package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit network/i uci set network.lan.ifname='eth1 eth2 eth3'"  package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit network/i uci set network.wan.ifname='eth0'"  package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit network/i uci set network.wan6.ifname='eth0'"  package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit network/i uci delete network.wan6"  package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit network/i uci delete network.lan.ip6assign"  package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit network/i uci delete network.globals.ula_prefix"  package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit system/a uci commit dhcp"  package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit dhcp/i uci delete dhcp.lan.ra"  package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit dhcp/i uci delete dhcp.lan.dhcpv6"  package/lean/default-settings/files/zzz-default-settings
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i 's/255.255.0.0/255.255.255.0/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# DIAG
+sed -i "/uci commit system/a uci commit diag"  package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit diag/i uci set luci.diag.dns='jd.com'"  package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit diag/i uci set luci.diag.ping='jd.com'"  package/lean/default-settings/files/zzz-default-settings
+sed -i "/uci commit diag/i uci set luci.diag.route='jd.com'"  package/lean/default-settings/files/zzz-default-settings
+
+# 隐藏首页显示用户名(by:kokang)
+sed -i 's/name="luci_username" value="<%=duser%>"/name="luci_username"/g' feeds/luci/modules/luci-base/luasrc/view/sysauth.htm
+sed -i 's/name="luci_username" value="<%=duser%>"/name="luci_username"/g' feeds/kenzo/luci-theme-argonne/luasrc/view/themes/argonne/sysauth.htm
+# 移动光标至第一格(by:kokang)
+sed -i "s/'luci_password'/'luci_username'/g" feeds/luci/modules/luci-base/luasrc/view/sysauth.htm
+sed -i "s/'luci_password'/'luci_username'/g" feeds/kenzo/luci-theme-argonne/luasrc/view/themes/argonne/sysauth.htm
 
 # Modify system
 sed -i 's/OpenWrt/Way/g' package/base-files/files/bin/config_generate
@@ -73,19 +107,6 @@ sed -i "s/OpenWrt /Way Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/
 #uci set network.lan.ip6assign='60'
 #uci commit
 #EOF
-
-# LAN LAN LAN WAN
-sed -i "/uci commit system/a uci commit network"  package/lean/default-settings/files/zzz-default-settings
-sed -i "/uci commit network/i uci set network.lan.ifname='eth1 eth2 eth3'"  package/lean/default-settings/files/zzz-default-settings
-sed -i "/uci commit network/i uci set network.wan.ifname='eth0'"  package/lean/default-settings/files/zzz-default-settings
-sed -i "/uci commit network/i uci set network.wan6.ifname='eth0'"  package/lean/default-settings/files/zzz-default-settings
-sed -i "/uci commit network/i uci delete network.wan6"  package/lean/default-settings/files/zzz-default-settings
-sed -i "/uci commit network/i uci delete network.lan.ip6assign"  package/lean/default-settings/files/zzz-default-settings
-sed -i "/uci commit network/i uci delete network.globals.ula_prefix"  package/lean/default-settings/files/zzz-default-settings
-
-sed -i "/uci commit system/a uci commit dhcp"  package/lean/default-settings/files/zzz-default-settings
-sed -i "/uci commit dhcp/i uci delete dhcp.lan.ra"  package/lean/default-settings/files/zzz-default-settings
-sed -i "/uci commit dhcp/i uci delete dhcp.lan.dhcpv6"  package/lean/default-settings/files/zzz-default-settings
 
 # TTYD AS ROOT AND OPENPORT
 sed -i "/uci commit system/a uci commit ttyd"  package/base-files/files/bin/config_generate
@@ -159,8 +180,6 @@ sed -i 's/150/250/g' package/network/services/dnsmasq/files/dhcp.conf
 # Password
 sed -i 's/root::0:0:99999:7:::/root:$1$P4yrmMQf$XRoELeUToXNeituE0pl22.:19131:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-########### 更改大雕源码（可选）###########
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
 
 # 设置密码为空
-#sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$rOlqcfTl$sQ03k9uRqA\/xTm7pzAmSs1:19130:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings   
+#sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$rOlqcfTl$sQ03k9uRqA\/xTm7pzAmSs1:19130:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings    
