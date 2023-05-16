bbfc31823efe29febfdc743ba353163b4b7307bc
wanjiban <109531247+wanjiban@users.noreply.github.com>
2023-05-16 21:23:44 +0800

    Update diy-part1.sh
    kiddin9

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,7 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages-backup' >>feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 
 #更改主题包源
 #rm -rf package/lean/luci-theme-argonne
